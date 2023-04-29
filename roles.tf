de73d33f4b8b34cc2863971eabdf313be3ec4392
saipranayteja <pranayteja.ch@algonox.com>
2023-04-29 21:40:28 +0530

    wsedf

--- roles.tf
+++ roles.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-    depends_on = [ aws_spot_instance_request.name_2 ]
+
   name               = "eks-cluster-example"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
