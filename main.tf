e97277a2e020bc7cde437f0822bc5557d33f2064
saipranayteja <pranayteja.ch@algonox.com>
2023-04-30 10:39:59 +0530

    ih

--- main.tf
+++ main.tf
@@ -1,27 +1,16 @@
-
-
-resource "aws_spot_instance_request" "name_2" {
-    ami           = "ami-0bb6af715826253bf"
-    instance_type = "t3.small"
-    spot_type     = "persistent"
-    instance_interruption_behavior = "stop"
-    wait_for_fulfillment=true
-    vpc_security_group_ids = ["sg-036e9bfb37a180657"]
-    iam_instance_profile = aws_iam_instance_profile.demo-profile.name
-
-  
-
-    tags = {
+resource "aws_instance" "demo-instance" {
+  ami = "ami-0bb6af715826253bf"
+  instance_type = "t3a.small"
+  vpc_security_group_ids = ["sg-036e9bfb37a180657"]
+  iam_instance_profile = aws_iam_instance_profile.demo-profile.name
+  tags = {
       Name = "Instance-1"
-    }
-
-    timeouts {
-    create = "10m"
-    delete = "10m"
   }
+
+
 provisioner "remote-exec" {
   connection {
-    host = aws_spot_instance_request.name_2.public_ip
+    host = aws_instance.demo-instance.public_ip
     user = "centos"
     password = "DevOps321"
   }
@@ -36,11 +25,8 @@ provisioner "remote-exec" {
   ]
   
 }
-
-
-
 }
 
 output "public-ip"{
-  value=aws_spot_instance_request.name_2.public_ip
+  value = aws_instance.demo-instance.public_ip
 }
