169d105932aaf33ecde0d6911dbc2999a64f88fe
saipranayteja <pranayteja.ch@algonox.com>
2023-04-30 09:05:57 +0530

    sdf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ provisioner "remote-exec" {
   }
 
   inline = [
-    "sudo git pull https://github.com/sai-pranay-teja/docker-and-kubernetes-practise.git",
+    "sudo git clone https://github.com/sai-pranay-teja/docker-and-kubernetes-practise.git",
     "sudo bash docker-and-kubernetes-practise/docker-install.sh",
     "sudo bash docker-and-kubernetes-practise/kubectl.sh",
     "sudo bash docker-and-kubernetes-practise/aws-iam-authenticator.sh"
