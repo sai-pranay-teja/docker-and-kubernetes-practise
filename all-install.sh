9acddd1238cf6641981632fc37ab3b0efd66946e
saipranayteja <pranayteja.ch@algonox.com>
2023-05-01 01:32:31 +0530

    sd

--- all-install.sh
+++ all-install.sh
@@ -1,4 +1,4 @@
-sudo git clone https://github.com/sai-pranay-teja/docker-and-kubernetes-practise.git
+
 sudo bash docker-and-kubernetes-practise/docker-install.sh
 sudo bash docker-and-kubernetes-practise/kubectl.sh
 sudo bash docker-and-kubernetes-practise/aws-iam-authenticator.sh
