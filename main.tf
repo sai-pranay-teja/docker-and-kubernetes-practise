d67664d13ce800e4cc05d485913218d44492cfd9
saipranayteja <pranayteja.ch@algonox.com>
2023-05-01 15:58:31 +0530

    sdf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_spot_instance_request" "name_2" {
-    ami           = "ami-0bb6af715826253bf"
+    ami           = "ami-0b5a2b5b8f2be4ec2"
     instance_type = "t3.small"
     spot_type     = "persistent"
     instance_interruption_behavior = "stop"
